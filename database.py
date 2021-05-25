5914aad6ea36c8897a0b063d5ccb5efe0e1e9d67
Alain <77918121+infotechbro@users.noreply.github.com>
2021-05-25 11:25:14 +0530

    Initial commit

--- database.py
+++ database.py
@@ -11,9 +11,13 @@ from ChatBot import (
 class Data(BASE):
     __tablename__ = "Data"
     chat_id = Column(String(14), primary_key=True)
+    message = Column(Integer)
+    ha_id = Column(Integer)
 
-    def __init__(self, chat_id):
+    def __init__(self, chat_id, message, ha_id):
         self.chat_id = chat_id
+        self.message = message
+        self.ha_id = ha_id
         
 Data.__table__.create(checkfirst=True)
 
@@ -21,3 +25,10 @@ def users():
     nah = SESSION.query(Data).all()
     SESSION.close()
     return nah
+
+def hmm_id(message_id: int):
+    try:
+        pro_id = SESSION.query(Users).get(str(message))
+        return int(pro_id.chat_id), pro_id.ha_id
+    finally:
+        SESSION.close()
